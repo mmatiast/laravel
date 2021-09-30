4016a43a2dc90524c67cf0d3fcd22168d017c841
Matias <mmatiast@miumg.edu.gt>
2021-09-29 22:17:01 -0600

    vue

---
+++ app/vue/01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'prueba con vue',
+        frutas: [
+            {nombre: 'pera', cantidad:10},
+            {nombre: 'mazana', cantidad:0},
+            {nombre: 'platano', cantidad:11}
+        ]
+    }
+})
